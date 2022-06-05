386a3167bba1feb93a7a20bd1ef9eae796f72a30
Bruno Leão <leaodebrito@icloud.com>
2022-06-05 16:01:22 -0300

    desenvolvimento de exercícios

--- Modulo 2/PE1_modulo 2/exercicio1.py
+++ Modulo 2/PE1_modulo 2/exercicio1.py
@@ -1,1 +1,2 @@
-print("heloo world")
+print("hello world")
+print("hello world", "- bye, world")
---
+++ Modulo 2/PE1_modulo 2/exercicio3.py
@@ -0,0 +1,2 @@
+print("The itsy bitsy spider \n climbed up the waterspout.")
+print("Down came the rain and \n washed the spider out.")
